07d53ee8c2ecb1ba8d928e2081b3fd1a6af50ab9
neosanora <neosanora@gmail.com>
2025-07-16 22:10:37 +0700

    Update arch.sh

--- share/packages/arch.sh
+++ share/packages/arch.sh
@@ -3,34 +3,47 @@
 # ----------------------------------------------------------
 
 packages=(
-    "wget"
-    "unzip"
-    "git"
-    "gum"    
-    "hyprland"
-    "waybar"
-    "rofi-wayland"
-    "kitty"
-    "dunst"
-    "thunar"
-    "xdg-desktop-portal-hyprland"
-    "qt5-wayland"
-    "qt6-wayland"
-    "hyprpaper"
-    "hyprlock"
-    "firefox"
-    "ttf-font-awesome"
-    "vim"
-    "nvtop"
-    "fastfetch"
-    "ttf-fira-sans" 
-    "ttf-fira-code" 
-    "ttf-firacode-nerd"
-    "jq"
-    "brightnessctl"
-    "networkmanager"
-    "pavucontrol"
-    "wireplumber"
-    "wlogout"
-    "flatpak"
+  # === Tools Umum ===
+  "wget"
+  "unzip"
+  "git"
+  "jq"
+  "vim"
+  "gum"
+  "fastfetch"
+
+  # === Font ===
+  "ttf-font-awesome"
+  "ttf-fira-sans"
+  "ttf-fira-code"
+  "ttf-firacode-nerd"
+
+  # === Display Manager & WM ===
+  "hyprland"
+  "xdg-desktop-portal-hyprland"
+  "qt5-wayland"
+  "qt6-wayland"
+
+  # === Aplikasi GUI & System ===
+  "kitty"           # terminal
+  "thunar"          # file manager
+  "firefox"         # browser
+  "pavucontrol"     # audio control
+  "dunst"           # notifikasi
+  "wlogout"         # logout dialog
+  "flatpak"         # package manager alternatif
+
+  # === Audio / Media ===
+  "wireplumber"
+  "brightnessctl"
+  "networkmanager"
+
+  # === Bar, Menu, Lock ===
+  "waybar"          # status bar
+  "rofi-wayland"    # app launcher
+  "hyprpaper"       # wallpaper
+  "hyprlock"        # lock screen
+
+  # === Monitoring ===
+  "nvtop"           # GPU usage
 )
