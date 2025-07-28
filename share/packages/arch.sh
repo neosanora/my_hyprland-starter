4909a0452ee96f59fce5f74f6c8db2ba226d64b2
neosanora <neosanora@gmail.com>
2025-07-28 14:04:20 +0000

    Update arch.sh

--- share/packages/arch.sh
+++ share/packages/arch.sh
@@ -1,61 +1,8 @@
-# ----------------------------------------------------------
-# Packages
-# ----------------------------------------------------------
-
-packages=(
-  # === Tools Umum ===
-  "wget"
-  "unzip"
-  "git"
-  "jq"
-  "vim"
-  "neovim"
-  "gum"
-  "fastfetch"
-
-  # === Font ===
-  "ttf-font-awesome"
-  "ttf-fira-sans"
-  "ttf-fira-code"
-  "ttf-firacode-nerd"
-
-  # === Display Manager & WM ===
-  "hyprland"
-  "xdg-desktop-portal-hyprland"
-  "qt5-wayland"
-  "qt6-wayland"
-
-  # === Aplikasi GUI & System ===
-  "kitty"           # Default terminal emulator
-  "alacritty"       # GPU-accelerated terminal emulator
-  "thunar"          # file manager
-  "firefox"         # browser
-  "pavucontrol"     # audio control
-  "dunst"           # notifikasi
-  "wlogout"         # logout dialog
-  "flatpak"         # package manager alternatif
-
-  # === Audio / Media ===
-  "wireplumber"
-  "brightnessctl"
-  "networkmanager"
-
-  # === Bar, Menu, Lock ===
-  "waybar"          # status bar
-  "rofi-wayland"    # app launcher
-  "hyprpaper"       # wallpaper
-  "hyprlock"        # lock screen
-
-  # === Monitoring ===
-  "nvtop"           # GPU usage
-
-)
-
 # ============================================
 # 🎯 Daftar Package
 # ============================================
 
-packagesNew=(
+packages=(
   #////////////////#
   ### Tools Umum ###
   #////////////////#
