9544a2f01bb7f4812bf61ebe1016e14f01abab4c
neosanora <neosanora@gmail.com>
2025-07-28 14:02:18 +0000

    Update arch.sh

--- share/packages/arch.sh
+++ share/packages/arch.sh
@@ -56,68 +56,96 @@ packages=(
 # ============================================
 
 packagesNew=(
-  # === Tools Umum ===
+  #////////////////#
+  ### Tools Umum ###
+  #////////////////#
   "firefox"         # Browser
   "git"             # version control
   "jq"              # JSON processor
   "gum"             # command line UI toolkit
   "fastfetch"       # system information tool
 
-  # === File Editor ===
+  #/////////////////#
+  ### File Editor ###
+  #/////////////////#
   "vim"             # text editor
   "neovim"          # modern text editor
 
-  # === File Manager ===
+  #//////////////////#
+  ### File Manager ###
+  #//////////////////#
   "ranger"          # File Manager Like NVIM
   "thunar"          # File manager ringan (XFCE)
 
-
-  # === App Manager ===
+  #/////////////////#
+  ### App Manager ###
+  #/////////////////#
   "flatpak"         # Alternatif package manager (sandboxed)
 
-  # === Font ===
+  #//////////#
+  ### Font ###
+  #//////////#
   "ttf-font-awesome"
   "ttf-fira-sans"
   "ttf-fira-code"
   "ttf-firacode-nerd"
 
-  # === Display Manager & WM ===
+  #//////////////////////////#
+  ### Display Manager & WM ###
+  #//////////////////////////#
   "hyprland"                      # Wayland compositor
   "xdg-desktop-portal-hyprland"   # xdg portal untuk Hyprland
   "qt5-wayland"                   # Qt support for Wayland
   "qt6-wayland"                   # Qt support for Wayland
 
-  # === Aplikasi GUI & System ===
+  #///////////////////////////////#
+  ### Terminal Emulator & Shell ###
+  #///////////////////////////////#
   "kitty"           # Terminal modern dan cepat
   "alacritty"       # Terminal GPU-accelerated
+
+  #///////////////////////////#
+  ### Aplikasi GUI & System ###
+  #///////////////////////////#
   "pavucontrol"     # GUI audio control
   "qjackctl"        # GUI control untuk JACK server
   "dunst"           # Sistem notifikasi ringan
   "brightnessctl"   # Kontrol kecerahan
 
-  # === Network ===
+  #/////////////#
+  ### Network ###
+  #/////////////#
   "networkmanager"  # Manajemen jaringan (WiFi/Ethernet)
   "curl"            # command line tool and library for transferring data with URLs
   "wget"            # Network utility to retrieve files from the web
 
-  # === Audio / Media ===
+  #///////////////////#
+  ### Audio / Media ###
+  #///////////////////#
   "pipewire-jack"   # JACK support via PipeWire
   "pipewire-alsa"   # ALSA support via PipeWire
   "pipewire-pulse"  # PulseAudio replacement via PipeWire
   "wireplumber"     # Session manager PipeWire
   "pamixer"         # CLI audio mixer
 
-  # === Bar, Menu, Lock ===
+  #/////////////////////#
+  ### Bar, Menu, Lock ###
+  #/////////////////////#
+
   "waybar"          # Widget bar / status bar
   "rofi-wayland"    # Wayland app launcher
   "hyprpaper"       # Wallpaper manager
   "wlogout"         # GUI logout menu
   "hyprlock"        # Lock screen Hyprland
 
-  # === Screenshot ===
+  #////////////////#
+  ### Screenshot ###
+  #////////////////#
   "grim"            # Alat screenshot untuk Wayland
   "slurp"           # Area selector (buat grim)
 
-  # === Monitoring ===
+  #////////////////#
+  ### Monitoring ###
+  #////////////////#
   "nvtop"           # Monitor GPU usage (mirip htop)
 )
